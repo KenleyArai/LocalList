8edf1a6bcffd1fd251cebe557cb4b7c2b8fb11b1
Kenley Arai <me@kenleyarai.com>
2017-01-20 22:36:29 -0800

    Going to convert Comment tutorial to todolist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
 
-import Routes from './routes';
-import './index.css';
+import CommentBox from './CommentBox';
 
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+  <CommentBox 
+      url='http://localhost:3001/api/comments'
+      pollInterval={2000} />,
   document.getElementById('root')
 );
