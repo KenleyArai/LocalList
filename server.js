44aacc9c11ba52749000e637a385ee3197185087
Kenley Arai <me@kenleyarai.com>
2017-01-20 23:25:49 -0800

    Added env var

--- server.js
+++ server.js
@@ -14,71 +14,83 @@ var router = express.Router();
 //it up, or 3001
 var port = process.env.API_PORT || 3001;
 
-mongoose.connect('mongodb://kenley:217217@ds056009.mlab.com:56009/local_list');
+mongoose.connect(process.env.DATABASE_URL);
 
 //now we should configure the API to use bodyParser and look for 
 //JSON data in the request body
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 //To prevent errors from Cross Origin Resource Sharing, we will set 
 //our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//and remove cacheing so we get the most recent comments
- res.setHeader('Cache-Control', 'no-cache');
- next();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    //and remove cacheing so we get the most recent comments
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
 });
 //now we can set the route path & initialize the API
-router.get('/', function(req, res) {
- res.json({ message: 'API Initialized!'});
-});
-
-router.route('/comments').get(function(req, res){
-    Comment.find(function(err, comments){
-        if(err)
-            res.send(err);
-        res.json(comments)
+router.get('/', function (req, res) {
+    res.json({
+        message: 'API Initialized!'
     });
-})
-.post(function(req, res) {
-    var comment = new Comment();
-    comment.author = req.body.author;
-    comment.text = req.body.text;
-    comment.save(function(err) {
-        if(err)
-            res.send(err);
-        res.json({message: 'Comment successfully added!'});
-    }); 
 });
 
-router.route('/comments/:comment_id')
-    .put(function(req, res) {
-        Comment.findById(req.params.comment_id, function(err, comment){
+router.route('/comments').get(function (req, res) {
+        Comment.find(function (err, comments) {
             if (err)
                 res.send(err);
-            (req.body.author) ? comment.author = req.body.author : null;
-            (req.body.text) ? comment.text = req.body.text : null;
-            comment.save(function(err) {
+            res.json(comments)
+        });
+    })
+    .post(function (req, res) {
+        var comment = new Comment();
+        comment.author = req.body.author;
+        comment.text = req.body.text;
+        comment.save(function (err) {
+            if (err)
+                res.send(err);
+            res.json({
+                message: 'Comment successfully added!'
+            });
+        });
+    });
+
+router.route('/comments/:comment_id')
+    .put(function (req, res) {
+        Comment.findById(req.params.comment_id, function (err, comment) {
+            if (err)
+                res.send(err);
+            (req.body.author) ? comment.author = req.body.author: null;
+            (req.body.text) ? comment.text = req.body.text: null;
+            comment.save(function (err) {
                 if (err)
                     res.send(err);
-                res.json({ message: 'Comment has been updated' });
+                res.json({
+                    message: 'Comment has been updated'
+                });
             });
         });
     })
-    .delete(function(req, res) {
-        Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+    .delete(function (req, res) {
+        Comment.remove({
+            _id: req.params.comment_id
+        }, function (err, comment) {
             if (err)
                 res.send(err);
-            res.json({ message: 'Comment has been deleted' })
+            res.json({
+                message: 'Comment has been deleted'
+            })
         })
     });
 
 //Use our router configuration when we call /api
 app.use('/api', router);
 //starts the server and listens for requests
-app.listen(port, function() {
- console.log(`api running on port ${port}`);
+app.listen(port, function () {
+    console.log(`api running on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import ReactDOM from 'react-dom';
 // TODO: Convert to todo form
 import CommentBox from './CommentBox';
 
-ReactDOM.render(
-  <CommentBox 
-      url='http://localhost:3001/api/comments'
-      pollInterval={2000} />,
+ReactDOM.render( <
+  CommentBox url = 'http://localhost:3002/api/comments'
+  pollInterval = {
+    2000
+  }
+  />,
   document.getElementById('root')
 );
